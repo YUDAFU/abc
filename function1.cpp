05813a7db14e8d73986e4eb43947313d2fdb47ea
jyl <2453366841@qq.com>
2018-09-14 17:17:11 +0800

    few changes

--- function1.cpp
+++ function1.cpp
@@ -1,6 +1,8 @@
 #include"function1.h"
 #include<stdio.h>
 
+static uint8_t abc = 123;
+
 static int func_internal_init()
 {
 	static int start = 0;
@@ -10,14 +12,40 @@ static int func_internal_init()
 	}
 
 	start++;
-	printf("start code:%d\n",start);
+	abc++;
+	printf("start code:%d, abc:%d\n", start, abc);
+	printf("ptr_start:%p, ptr_abc:%p\n", &start, &abc);
 
 	return 0;
 }
 
 int func_get()
 {
-	printf("func_get\n");
+	printf("func_get#####################\n");
+
+	uint8_t *test_data = 0;
+	printf("%p\n", test_data++);
+	printf("%p\n", test_data++);
+	printf("%p\n", test_data++);
+	
+	int *test_data1 = 0;
+	printf("%p\n", test_data1++);
+	printf("%p\n", test_data1++);
+	printf("%p\n", test_data1++);
+	
+	int test_data2 = 30;
+	int *ptr_data = &test_data2;
+	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
+	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
+	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
+	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
+	
+	uint8_t test_data3 = 40;
+	uint8_t *ptr_data1 = &test_data3;
+	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
+	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
+	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
+	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
 
       return 0;	
 }
@@ -26,7 +54,7 @@ int func_get()
 
 int func_pop()
 {
-	printf("func_pop\n");
+	printf("func_pop#####################\n");
 	
 	uint8_t func_data = 66;
 
--- main.cpp
+++ main.cpp
@@ -3,35 +3,10 @@
 #include<stdint.h>
 #include"function1.h"
 
-static uint8_t abc = 123;
 
 int main()
 {
-	uint8_t *test_data = 0;
-	printf("%p\n", test_data++);
-	printf("%p\n", test_data++);
-	printf("%p\n", test_data++);
-	
-	int *test_data1 = 0;
-	printf("%p\n", test_data1++);
-	printf("%p\n", test_data1++);
-	printf("%p\n", test_data1++);
-
-	int test_data2 = 30;
-	int *ptr_data = &test_data2;
-	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
-	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
-	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
-	printf("%p %u %d\n", ptr_data++, ptr_data, *ptr_data);
-
-	uint8_t test_data3 = 40;
-	uint8_t *ptr_data1 = &test_data3;
-	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
-	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
-	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
-	printf("%p %u %d\n", ptr_data1++, ptr_data1, *ptr_data1);
-
-	int i = 0;
+	int i;
 	for(i = 0; i <= 9; i++){
 		func_configure();
 	}
